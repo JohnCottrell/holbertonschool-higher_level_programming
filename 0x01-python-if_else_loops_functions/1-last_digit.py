e1a7ae6fd7c783ae30bd0d27c63fd16df79e8c2a
JohnCottrell <johnpcottrell@gmail.com>
2017-08-01 22:34:10 +0000

    remove zero condition, add less than or equal to zero condition.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
+if number >= 0:
     lastdigit = number % 10
 elif number < 0:
     lastdigit = number % -10
-elif lastdigit == 0:
-    lastdigit = 0
 if lastdigit > 5:
     str = "and is greater than 5"
 elif lastdigit == 0:
