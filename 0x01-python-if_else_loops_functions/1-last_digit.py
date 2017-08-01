bb9c7453c03cfdf0371a07bfe5a2cdc17cd111ae
JohnCottrell <johnpcottrell@gmail.com>
2017-08-01 22:24:15 +0000

    fix case with zero.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,12 +5,12 @@ if number > 0:
     lastdigit = number % 10
 elif number < 0:
     lastdigit = number % -10
-elif number == 0:
-    lastdigit = number
+elif lastdigit == 0:
+    lastdigit = 0
 if lastdigit > 5:
     str = "and is greater than 5"
-elif lastdigit < 6:
-    str = "and is less than 6 and not 0"
 elif lastdigit == 0:
     str = "and is 0"
+elif lastdigit < 6:
+    str = "and is less than 6 and not 0"
 print("Last digit of", number, "is", lastdigit, str)
