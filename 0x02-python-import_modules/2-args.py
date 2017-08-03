b0318a2d221bc8641fffe797333ab26cfb569f1b
JohnCottrell <johnpcottrell@gmail.com>
2017-08-03 23:35:54 +0000

    adjust numbering by subtracting one from number of arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     numarg = len(argv)
-    print("{}".format(numarg), end="")
+    print("{}".format(numarg - 1), end="")
     if numarg == 1:
         print(" argument.")
     elif numarg == 2:
