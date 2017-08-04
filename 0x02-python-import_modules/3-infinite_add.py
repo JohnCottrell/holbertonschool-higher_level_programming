28122bac8a79f5de181a7a5dcf0fcdc26900fda5
JohnCottrell <johnpcottrell@gmail.com>
2017-08-04 00:14:09 +0000

    simplify addition.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     numarg = len(argv)
-    prevnum = 0
+    num = 0
     for i in range (1, numarg):
-        num = int(argv[i])
-        num += prevnum
-        prevnum = num
+        num += int(argv[i])
     print(num)
