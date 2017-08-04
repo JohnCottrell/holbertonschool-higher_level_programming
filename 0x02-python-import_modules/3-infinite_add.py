c14d50c1f122269c97f9bf88cf6638e57f82a4bb
JohnCottrell <johnpcottrell@gmail.com>
2017-08-04 18:11:51 +0000

    remove whitespace after range.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     numarg = len(argv)
     num = 0
-    for i in range (1, numarg):
+    for i in range(1, numarg):
         num += int(argv[i])
     print("{}".format(num))
