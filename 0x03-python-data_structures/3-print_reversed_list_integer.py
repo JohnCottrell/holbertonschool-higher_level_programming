bc9f1097b1e9de6dc49d6eeda417c3ae22a540f3
JohnCottrell <johnpcottrell@gmail.com>
2017-08-08 22:10:43 +0000

    multiple changes including change of print format and subtracting one from index.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = len(my_list)
-    for i in range idx:
-        print{[:d}.format(my_list[idx]))
+    idx = len(my_list) - 1
+    for i in my_list:
+        print("{:d}".format(my_list[idx]))
         idx = idx - 1
